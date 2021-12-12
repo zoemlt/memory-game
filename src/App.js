1291d0712e6ffd341fd8c21ccfca3f62fcc1689a
zoe <53597160+zoeniu@users.noreply.github.com>
2021-12-13 11:11:37 +1300

    Display cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import SingleCard from './components/SingleCard'
+
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" }
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({...card, id: Math.random()}))
+
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard card={card} key={card.id}/>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
